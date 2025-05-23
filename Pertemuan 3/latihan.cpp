1d3360f9aa230af361b022926f5c2d6afd469e30
Farhan00755 <tyoprasetyo2007@gmail.com>
2025-05-23 19:52:11 +0700

    latihan.cpp

--- Pertemuan 3/latihan.cpp
+++ Pertemuan 3/latihan.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // header untuk cpp
+using namespace std; // memudahkan pengkodingan agar tidak mendeklarasikan std:: lagi
 
 int main() {
     int bil;
-    system("cls");
+    system("cls"); // untuk menghapus yang tidak penting pada terminal agar terlihat bersih 
     
     cout << "Masukkan bilangan : ";
     cin >> bil;
 
-    if (bil % 5 == 0 && bil % 10 == 0) {
+    if (bil % 5 == 0 && bil % 10 == 0) { // jika sisa bagi bil mod 5 sama dengan 0 dan sisa bagi bil mod 10 sana dengan 0 menjalankan perintah yang dibawah
         cout << "Bilangan " << bil << " adalah kelipatan 10 dan 5" << endl;
-    } else if (bil % 10 == 0) {
+    } else if (bil % 10 == 0) { // jika sisa bagi bil mod 10 sana dengan 0 menjalankan perintah yang dibawah
         cout << "Bilangan " << bil << " adalah kelipatan 10" << endl;
-    } else if (bil % 5 == 0) {
+    } else if (bil % 5 == 0) { // jika sisa bagi bil mod 0 sana dengan 0 menjalankan perintah yang dibawah
         cout << "Bilangan " << bil << " adalah kelipatan 5" << endl;
-    } else {
+    } else { // jika tidak memenuhi ketiga syarat diatas
         cout << "Bilangan " << bil << " bukan kelipatan 5 atau 10" << endl;
     }
 }
