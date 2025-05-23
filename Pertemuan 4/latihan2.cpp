6a141f2e522fcedcee7763cae8b2f031471bd0b3
Farhan00755 <tyoprasetyo2007@gmail.com>
2025-05-24 06:52:59 +0700

    latihan2.cpp

--- Pertemuan 4/latihan2.cpp
+++ Pertemuan 4/latihan2.cpp
@@ -11,7 +11,7 @@ int main() {
     getline(cin, kalimat);
 
     for (i = 0; i < kalimat.length(); i++) {
-        if (kalimat[i] == 'a' || kalimat[i] == 'e' || kalimat[i] == 'i' || kalimat[i] == 'o' || kalimat[i] == 'u') {
+        if (kalimat[i] == 'a' || kalimat[i] == 'e' || kalimat[i] == 'i' || kalimat[i] == 'o' || kalimat[i] == 'u') { // looping ini hanya merubah kalimat i jika kalimatnya a,i,u,e,o atau kalimat vokal selebihnya tidak diubah
             kalimat[i] = toupper(kalimat[i]);
         }
     }
