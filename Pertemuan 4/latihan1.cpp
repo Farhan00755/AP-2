4eed9d632d10c124b0541a3e1347853be92da1a6
Farhan00755 <tyoprasetyo2007@gmail.com>
2025-05-24 06:51:23 +0700

    latihan1.cpp

--- Pertemuan 4/latihan1.cpp
+++ Pertemuan 4/latihan1.cpp
@@ -8,14 +8,14 @@ int main() {
     system("cls");
 
     cout << "Masukkan kalimat : ";
-    getline(cin, kalimat);
+    getline(cin, kalimat); // getline adalah perintah yang dapat menerima spasi
 
     // farhan
-    for (i = 0; i < kalimat.length(); i++) {
-        kalimat[i] = toupper (kalimat[i]);
+    for (i = 0; i < kalimat.length(); i++) { // kalimat.length untuk mengetahui panjang kalimat misalnya KOTA panjang kalimatnya 4 jadi loop for akan berjalan selama i lebih kecil dari 4
+        kalimat[i] = toupper (kalimat[i]); // kalimat ke i akan di capslock semua 
     }
 
-    cout << "Kalimat dalam huruf kapital : " << kalimat << endl;
+    cout << "Kalimat dalam huruf kapital : " << kalimat << endl; // menampilkan kalimat yang sudah di ubah menjadi huruf besar
     //FARHAN 
     
     return 0;
