32fdb42f8065f6ed08e6458590f7a1fd5030afd3
Farhan00755 <tyoprasetyo2007@gmail.com>
2025-05-24 15:50:56 +0700

    headerKustom.h

--- Pertemuan 6/headerKustom.h
+++ Pertemuan 6/headerKustom.h
@@ -1,5 +1,5 @@
-#define L << 
-#define u using namespace std
-#define o cout
-#define e endl
-#define s system
+#define L << // mengubah deklarasi << menjadi L
+#define u using namespace std // mengubah deklarasi using namespace std menjadi u
+#define o cout // mengubah syntax cout menjadi o
+#define e endl // mengubah syntax endl menjadi e
+#define s system // mengubah syntax system menjadi s
