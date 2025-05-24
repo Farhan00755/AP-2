403aaf579827d3ea7dbe2c2aa16dd2930fc23289
Farhan00755 <tyoprasetyo2007@gmail.com>
2025-05-24 16:03:34 +0700

    Pointer.cpp

--- Pertemuan 7/Pointer.cpp
+++ Pertemuan 7/Pointer.cpp
@@ -5,8 +5,8 @@ void penjumlahan(int a, int b) {
     cout << a + b << endl;
 }
 
-void penjumlahanPointer(int *a, int *b) {
-    *a += *b;
+void penjumlahanPointer(int *a, int *b) { // tanda * sebagai deklarasi pointer
+    *a += *b; // menjumlahkan kedua variabel pointer dengan sort hand
     cout << *a << endl;
 }
 
@@ -15,10 +15,10 @@ int main() {
 
     // pointer declaration //
     int number = 35;
-    int *pointer_number = &number;
+    int *pointer_number = &number; // variabel pointer yang berisi alamat dari variabel number karena pada variabel normal simbol & untuk mengetahui alamat dari variabel tsb
 
     // cout << "isi variabel number = " << number << endl;
-    // cout << "alamat dari memori variabel number = " << &number << endl;
+    // cout << "alamat dari memori variabel number = " << &number << endl; 
     // cout << "isi variabel pointer_number = " << pointer_number << endl;
     // cout << "isi  variabel yang ditunjuk oleh pointer_number = " << *pointer_number << endl;
     // cout << "alamat memori variabel pointer_number = " << &pointer_number << endl;
@@ -36,8 +36,8 @@ int main() {
 
 
     // pointer in array //
-    int num[] = {1, 2, 3, 4, 5};
-    int *pointer_num = num;
+    int num[] = {1, 2, 3, 4, 5}; // variabel array normal
+    int *pointer_num = num; // variabel pointer berisi data variabel array normal
     // cout << "isi variabel num = " << num[0] << endl;
     // cout << "alamat dari memori variabel num = " << &num[3] << endl;
     // cout << "isi variabel pointer_num = " << pointer_num << endl;
@@ -75,11 +75,11 @@ int main() {
 
 
     // dynamic pointer //
-    int *ptr = new int;
-    *ptr = 30;
+    int *ptr = new int; // menambahkan memory baru yang dapat menampung integer
+    *ptr = 30; // isi pointer 30
     cout << "isi variabel ptr = " << *ptr << " dan alamat  " << &ptr << endl;
     
-    delete ptr;
+    delete ptr; // menghapus memory yang telah dibuat sebelumnya pada new int
     cout << "isi variabel ptr = " << *ptr << " dan alamat  " << &ptr << endl;
     
 }
