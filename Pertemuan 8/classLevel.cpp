b59b8604c26f719d1a57264930f91fad97bd7dd0
Farhan00755 <tyoprasetyo2007@gmail.com>
2025-05-24 16:20:27 +0700

    classLevel.cpp

--- Pertemuan 8/classLevel.cpp
+++ Pertemuan 8/classLevel.cpp
@@ -1,28 +1,28 @@
 #include<iostream>
 using namespace std;
 
-class ContohAkses {
-    private: 
+class ContohAkses { // deklarasi kelas untuk membagi tingkat variabel
+    private: // variabelnya hanya dapat diakses di dalam kelas yang sama
         int privateVar;
 
-    protected:
+    protected: // variabelnya hanya dapat diakses di dalam kelas yang sama dan kelas turunannya
         int protectedVar;
 
-    public:
+    public: // variabelnya dapat diakses dari mana saja
         int publicVar;
 
         //constructor
-        ContohAkses() {
-            privateVar = 1;
+        ContohAkses() { // memasukkan  nilai kedalam variabel yang telah dideklarasi
+            privateVar = 1; 
             protectedVar = 2;
             publicVar = 3;
         }
 
-    void TampilkanSemua() {
+    void TampilkanSemua() { // fungsi untuk menampilkan nilainya
         cout << "Akses dari dalam class : " << endl;
-        cout << privateVar << endl;
-        cout << protectedVar << endl;
-        cout << publicVar << endl;
+        cout << privateVar << endl; // dapat diakses karena masih berada dalam kelas yang sama
+        cout << protectedVar << endl; // dapat diakses
+        cout << publicVar << endl; // dapat diakses
     }
 };
 
@@ -30,8 +30,8 @@ class ContohAkses {
 class turunan : public ContohAkses {
     public:
     void aksesProtected() {
-        cout << "Akses publicvar " << publicVar << endl;
-        cout << "Akses protectedVar " << protectedVar << endl;
+        cout << "Akses publicvar " << publicVar << endl; // dapat diakses
+        cout << "Akses protectedVar " << protectedVar << endl; // dapat diakses karena ini adalah kelas turunannya
         // cout << "Akses privateVar " << privateVar << endl; // gak bisa di akses karena private cuma bisa di akses di classnya sendiri 
     }
 };
@@ -39,15 +39,15 @@ class turunan : public ContohAkses {
 int main() {
     system("cls");
 
-    ContohAkses obj;
-    obj.TampilkanSemua();
+    ContohAkses obj; // deklarasi variabel obj dengan tipe data kelas ContohAkses
+    obj.TampilkanSemua(); // memanggil kelas
 
     cout << "Akses dari luar kelas " << endl;
-    cout << obj.publicVar << endl;
+    cout << obj.publicVar << endl; // dapat diakses darimana saja
     // cout << obj.privateVar << endl; // hanya bisa diakses pada classnya sendiri
     // cout << obj.protectedVar << endl; // hanya bisa diakses dari classnya sendiri dan turunannya 
 
-    turunan tur;
-    tur.aksesProtected();
+    turunan tur; // deklarasi variabel tur dengan tipe data turunan dari kelas 
+    tur.aksesProtected(); // memanggil kelas turunan
 
 }
