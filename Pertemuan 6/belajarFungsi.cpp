ae7e14010803caaa06374040bd58788d04d7ab76
Farhan00755 <tyoprasetyo2007@gmail.com>
2025-05-24 15:40:58 +0700

    belajarFungsi.cpp

--- Pertemuan 6/belajarFungsi.cpp
+++ Pertemuan 6/belajarFungsi.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void sapa(string nama) {
+void sapa(string nama) { // deklarasi untuk fungsi dan variabelnya
     cout << "Halo " << nama <<" ! Selamat Datang di AP 2 !" << endl;
 }
 
 int main() {
-    string namaPengguna = "Farhan";
+    string namaPengguna = "Farhan"; // deklarasi nama user
 
-    sapa(namaPengguna);
+    sapa(namaPengguna); // memanggil fungsi yang sudah dibuat
 
     return 0;
 }
