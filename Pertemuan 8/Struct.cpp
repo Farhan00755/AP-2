40fea04b447711f8bdd7391823dd92212a1c06d0
Farhan00755 <tyoprasetyo2007@gmail.com>
2025-05-24 16:10:38 +0700

    Struct.cpp

--- Pertemuan 8/Struct.cpp
+++ Pertemuan 8/Struct.cpp
@@ -3,9 +3,9 @@
 #include<vector>
 using namespace std;
 
-struct Alamat {
-    string jalan, kota;
-    int kodePos;
+struct Alamat { // deklarasi struct yang dapat menampung banyak tipe data
+    string jalan, kota; // deklarasi variabel dengan tipe data string
+    int kodePos; // deklarasi variabel dengan tipe data integer
 };
 
 struct Mahasiswa {
@@ -18,9 +18,9 @@ struct Mahasiswa {
 int main() {
     system("cls");
 
-    Mahasiswa mhs1;
+    Mahasiswa mhs1; // deklarasi variabel mhs1 dengan struct Mahasiswa yang berisi banyak tipe data
 
-    // mhs1.nama = "Farhan";
+    // mhs1.nama = "Farhan"; // deklarasi dengan . untuk menunjuk ke variabel mana yang ingin disimpan
     // mhs1.umur = 18;
     // mhs1.ipk = 3.92;
 
@@ -30,16 +30,16 @@ int main() {
 
     // cout << "Alamat = " << mhs1.alamat.jalan << " " << mhs1.alamat.kota << " " << mhs1.alamat.kodePos << endl;
 
-    vector <Mahasiswa> mahasiswa;
+    vector <Mahasiswa> mahasiswa; // menggunakan vector untuk memasukkan data mahasiswa yang banyak
     int n;
     cout << "Masukkan banyak mahasiswa : ";
     cin >> n;
     cout << endl;
     
-    for(int i = 0; i < n; i++) { 
+    for(int i = 0; i < n; i++) { // deklarasi loop for untuk memasukkan data mahasiswa
         cout << "Mahasiswa " << i + 1 << endl;
 
-        cin.get();
+        cin.get(); // membalikan nilai
         cout << "Masukkan nama = ";
         getline(cin, mhs1.nama);
         cout << "Masukkan umur = ";
@@ -48,12 +48,12 @@ int main() {
         cin >> mhs1.ipk;
         cout << endl;
         
-        mahasiswa.push_back(mhs1);
+        mahasiswa.push_back(mhs1); // memasukkan data mahasiswa yang telah diinput kedalam vector
     }
     
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) { // deklarasi loop for untuk menampilkan data mahasiswa yang telah diinput
         cout << "Mahasiswa " << i + 1 << endl;
-        cout << "Nama : " << mahasiswa[i].nama << endl;
+        cout << "Nama : " << mahasiswa[i].nama << endl; // tata cara pemanggilan struct dalam vector dengan menambahkan [i] untuk mengetahui letak data dalam vector
         cout << "Umur : " << mahasiswa[i].umur << endl;
         cout << "IPK : " << mahasiswa[i].ipk << endl;
         cout << endl;
