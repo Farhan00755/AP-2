b525cc8aadef7f6015a198070b64c865d34b3bfe
Farhan00755 <tyoprasetyo2007@gmail.com>
2025-05-24 15:46:39 +0700

    variabel.cpp

--- Pertemuan 6/variabel.cpp
+++ Pertemuan 6/variabel.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-string namaGlobal = "Ilmu";
+string namaGlobal = "Ilmu"; // deklarasi variabel global yang dapat dipanggil baik dari proram utama dan sub program / fungsi
 
 void namaVariabel() {
-    string namaLokal = "Komputer";
+    string namaLokal = "Komputer"; // variabel yang dapat dipanggil hanya dalam fungsi ini saja
 
     //coba akses variabel lokal
     cout << namaLokal << endl;
