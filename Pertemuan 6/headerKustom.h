f30f298089a39e1683b0a3c51178b0d026a46ca6
Farhan00755 <tyoprasetyo2007@gmail.com>
2025-05-25 07:17:14 +0700

    Pertemuan 6 : Fungsi & Header

--- Pertemuan 6/headerKustom.h
+++ Pertemuan 6/headerKustom.h
@@ -1,5 +1,5 @@
 #define L << // mengubah deklarasi << menjadi L
-#define u using namespace std // mengubah deklarasi using namespace std menjadi u
+#define u using namespace std // mengubah deklarasi using namespace menjadi u
 #define o cout // mengubah syntax cout menjadi o
 #define e endl // mengubah syntax endl menjadi e
 #define s system // mengubah syntax system menjadi s
